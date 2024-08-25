1d6d968b4b5b2972dd08fe01302821bdb31cee5e
Marcus Vynicius <viniciusfla1999@gmail.com>
2024-08-26 00:04:08 +0100

    Exercicios com Switch Case + Loop + IF/ELSE

---
+++ Dia09/index.js
@@ -0,0 +1,40 @@
+let nome = prompt("Digite seu nome:");
+let idade = parseInt(prompt("Digite sua idade:"));
+let peso = parseInt(prompt("Digite seu peso:"));
+let altura = parseFloat(prompt("Digite sua altura:"));
+let profissão = prompt("Digite sua profissão:");
+
+console.log("Olá " + nome + ", você tem " + idade + " anos, é " + profissão + ", tem " + altura + "m de altura e pesa " + peso + "kg.");
+
+if(idade >= 18){
+    console.log("Está liberado para tomar uma gelada!");
+} else {
+    console.log("Sem gelada para você, volte quando tiver 18 anos.");
+} 
+
+const meses = Math.floor(idade * 12) ;
+const semanas = Math.floor(idade * 52) ;
+const dias = Math.floor(idade * 365) ;
+
+console.log("Você tem " + meses + " meses de vida.");
+console.log("Você tem " + semanas + " semanas de vida.");
+console.log("Você tem " + dias + " dias de vida.");
+
+let imcUsuario = peso / (altura*altura); 
+let totalImc = parseFloat(imcUsuario.toFixed(1));
+
+
+ if(totalImc < 18.5){ 
+    console.log("Seu IMC é: " + totalImc + " kg/m2 e você está abaixo do peso!");
+     
+} else if(totalImc >= 18.5 && totalImc <= 24.9){
+    console.log("Seu IMC é: " + totalImc + " kg/m2 e você está no peso ideal!");
+
+} else if(totalImc >= 24.9 && totalImc <= 30){
+    console.log("Seu IMC é; " + totalImc + " kg/m2 e você está acima do peso!");
+
+} else if(totalImc > 30){
+    console.log("Seu IMC é: " + totalImc + " kg/m2 e você está obeso!");
+}
+
+let anoNascimentoUsu = parseInt(2023 - idade); 
